d71bd92fbc0d38c29734d640498bc55fc741a269
Vituinho <trabalhovictor531@gmail.com>
2025-11-21 11:47:29 -0300

    Feat: Serviços e dependências

--- index.php
+++ index.php
@@ -5,7 +5,37 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = new \Slim\App(
+    ['settings' => ['displayErrorDetails' => true]]
+);
+
+/* Container dependency injection */
+class Servico {
+
+}
+
+/* Container Pimple */
+$container = $app->getContainer();
+$container['servico'] = function() {
+    return new Servico;
+};
+
+$app->get('/servico', function(Request $request, Response $response) {
+
+    $servico = $this->get('servico');
+    var_dump($servico);
+});
+
+/* Controllers como serviço */
+$container = $app->getContainer();
+$container['Home'] = function() {
+    return new MyApp\controllers\Home( new MyApp\View );
+};
+$app->get('/usuario', 'Home:index' );
+
+$app->run();
+
+/*
 
 $app->get('/postagens', function(Request $request, Response $response){
 
@@ -50,8 +80,6 @@ $app->delete('/usuarios/remove/{id}', function(Request $request, Response $respo
     return $response->getBody()->write("Sucesso ao deletar: " . $id);
 
 });
-
-$app->run();
 
 /*
 
